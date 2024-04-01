eb050bb293f0785981dc460d68ed4a6684ce22c6
Shaheer Ahmed <shaheer.ahmed@engie.com>
2024-04-01 15:23:52 +0400

    cleanup

--- data_modeling.py
+++ data_modeling.py
@@ -13,10 +13,35 @@ from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC, LinearSVC
-from sklearn.metrics import accuracy_score, confusion_matrix, recall_score, precision_score
+from sklearn.metrics import accuracy_score, confusion_matrix, recall_score, precision_score, f1_score
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
+
+
+def accuracy_scores(models):
+    accuracy = []
+    recall = []
+    precision = []
+    f1Score = []
+    train_time = []
+    
+    for model in models:
+        start_time = pd.datetime.now()
+        model.fit(X_train, y_train)
+        end_time = pd.datetime.now()
+        
+        y_pred = model.predict(X_test)
+        
+        accuracy.append(round(accuracy_score(y_test, y_pred),4))
+        recall.append(round(recall_score(y_test, y_pred),4))
+        precision.append(round(precision_score(y_test, y_pred),4))
+        f1Score.append(round(f1_score(y_test, y_pred),4))
+        train_time.append(end_time - start_time)
+    
+    return accuracy, recall, precision, f1Score, train_time
+
+
 
 data = pd.read_csv('Data/bank-full-preprocessed.csv')
 
@@ -42,20 +67,13 @@ X2 = data.drop('y', axis=1)
 # Train test split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-# Train test split
+# Train test split for stats models
 X2_train, X2_test, y2_train, y2_test = train_test_split(X2, y2, test_size=0.2, random_state=0)
-
-# Feature Scaling
-sc = StandardScaler()
-X_train[:,:8] = sc.fit_transform(X_train[:,:8])
-X_test[:,:8] = sc.transform(X_test[:,:8])
-
 
 # Stats models linear regression
 X2_sm = X2_train = sm.add_constant(X2_train)
 model = sm.OLS(y2_train, X2_sm).fit()
 model.summary()
-
 
 # Stats model without the less significant variables
 X3 = data.drop(['age','pdays','job_admin.','job_blue-collar','job_entrepreneur','job_housemaid','job_management','job_self-employed','job_services','job_technician','job_unemployed','default','y'], axis=1)
@@ -67,31 +85,19 @@ X3_sm = X3_train = sm.add_constant(X3_train)
 model = sm.OLS(y3_train, X3_sm).fit()
 model.summary()
 
-yhat = model.predict(X3_test)
-prediction = list(map(round, yhat))
 
-accuracy_score(y3_test, prediction)
+# Feature Scaling
+sc = StandardScaler()
+X_train[:,:8] = sc.fit_transform(X_train[:,:8])
+X_test[:,:8] = sc.transform(X_test[:,:8])
+
 
 # Linear Regression
 lr = LogisticRegression()
-lr.fit(X_train, y_train)
-y_pred = lr.predict(X_test)
-
-metrics_lr_acc = accuracy_score(y_test, y_pred)
-metrics_lr_recall = recall_score(y_test, y_pred)
-metrics_lr_prec = precision_score(y_test, y_pred)
-metrics_lr_conf_matrix = confusion_matrix(y_test, y_pred)
 
 
 # SVM C-Support Vector Machines
 svc = SVC()
-svc.fit(X_train, y_train)
-y_pred = svc.predict(X_test)
-
-metrics_svc_acc = accuracy_score(y_test, y_pred)
-metrics_svc_recall = recall_score(y_test, y_pred)
-metrics_svc_prec = precision_score(y_test, y_pred)
-metrics_svc_conf_matrix = confusion_matrix(y_test, y_pred)
 
 
 # SVM Linear SVC
@@ -104,17 +110,9 @@ for n in iterations:
     svm_acc.append(accuracy_score(y_test, y_pred))
 
 plt.plot(iterations, svm_acc)
-# max_iter of 400 gives the best results
+# max_iter of 300 gives the best results
 
-lsvc = LinearSVC(max_iter = n, random_state=0)
-lsvc.fit(X_train, y_train)
-y_pred = lsvc.predict(X_test)
-svm_acc.append(accuracy_score(y_test, y_pred))
-
-metrics_lsvc_acc = accuracy_score(y_test, y_pred)
-metrics_lsvc_recall = recall_score(y_test, y_pred)
-metrics_lsvc_prec = precision_score(y_test, y_pred)
-metrics_lsvc_conf_matrix = confusion_matrix(y_test, y_pred)
+lsvc = LinearSVC(max_iter = 300, random_state=0)
 
 
 # KNN Classification
@@ -128,7 +126,10 @@ for n in neighbors:
     acc.append(accuracy_score(y_test, y_pred))
 
 plt.plot(neighbors, acc)
+
 # n_neighbors tapers off after just 2 which makes sense since it's a binary classification model
+knn = KNeighborsClassifier(n_neighbors = 2)
+
 
 # Decision Tree Classifier
 criterion = ['gini','entropy']
@@ -140,12 +141,9 @@ for c in criterion:
     dt_acc.append(accuracy_score(y_test, y_pred))
 
 plt.plot(criterion, dt_acc)
-# Entropy seems to give the best results
 
-metrics_dt_acc = accuracy_score(y_test, y_pred)
-metrics_dt_recall = recall_score(y_test, y_pred)
-metrics_dt_prec = precision_score(y_test, y_pred)
-metrics_dt_conf_matrix = confusion_matrix(y_test, y_pred)
+# Entropy seems to give the best results
+dt = DecisionTreeClassifier(criterion='entropy')
 
 
 # Random Forest
@@ -158,14 +156,19 @@ for e in estimators:
     rf_acc.append(accuracy_score(y_test, y_pred))
 
 plt.plot(estimators, rf_acc)
+
 # n_estimators = 250 seems to give the best results
-
-metrics_rf_acc = accuracy_score(y_test, y_pred)
-metrics_rf_recall = recall_score(y_test, y_pred)
-metrics_rf_prec = precision_score(y_test, y_pred)
-metrics_rf_conf_matrix = confusion_matrix(y_test, y_pred)
-
-np.mean(cross_val_score(rf, X_train, y_train, scoring='neg_mean_absolute_error', cv=3))
+rf = RandomForestClassifier(n_estimators = 250, criterion='entropy', random_state=0)
 
 
+# Making an accuracy matrix to measure performance of all models
+models = [lr, svc, lsvc, knn, dt, rf]
+model_names = ['Linear Regression','SVC','Linear SVM','KNN','Decision Trees','Random Forrest']
+accuracy, recall, precision, f1Score, train_time = accuracy_scores(models)
+accuracy_matrix = pd.DataFrame({
+    'Accuracy':accuracy,
+    'Recall':recall,
+    'Precision':precision,
+    'F1 Score':f1Score,
+    'Training Time':train_time},index=model_names)
 
